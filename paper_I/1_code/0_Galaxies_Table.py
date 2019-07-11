f88911fabe7b8a7cd5200ba1acad1e56d31de59e
MixedPack <MixedPack@users.noreply.github.com>
2019-07-11 19:14:16 +0200

    Update 0_Galaxies_Table.py

--- paper_I/1_code/0_Galaxies_Table.py
+++ paper_I/1_code/0_Galaxies_Table.py
@@ -7,7 +7,8 @@ import math
 from numpy.polynomial.polynomial import polyfit
 
 #The list of Good SAMI_2015&2016 galaxies - with their FCC and FDS names
-with open( '../0_data/Literature/FCC_FDS.csv', 'r' ) as f:
+#with open( '../0_data/Literature/FCC_FDS.csv', 'r' ) as f:
+with open( '../0_data/tmp/NotChosen_FCC_FDS.csv', 'r' ) as f:
     name = csv.reader(f)
     name_list = list(map(tuple, name))
 
@@ -24,6 +25,7 @@ COV_mr_logRe2 = -0.00528375
 
 #importing information from FDS's catalog
 M_r = np.zeros(size)
+M_g = np.zeros(size)
 R_eff = np.zeros(size)
 mu_r = np.zeros(size)
 axis_ratio = np.zeros(size)
@@ -31,6 +33,7 @@ RA = np.zeros(size)
 DEC = np.zeros(size)
 log_mass = np.zeros(size)
 ERR_m_r = np.zeros(size)
+ERR_m_g = np.zeros(size)
 ERR_mu_r = np.zeros(size)
 ERR_R_eff = np.zeros(size)
 ERR_log_mass = np.zeros(size)
@@ -54,6 +57,8 @@ for l in range(size):
 			DEC[l] = tbdata_FDS.field('DEC')[j]
 			M_r[l] = tbdata_FDS.field('r_mag')[j] - 31.0
 			ERR_m_r[l] = tbdata_FDS.field('r_mag_e')[j]
+			M_g[l] = tbdata_FDS.field('g_mag')[j] - 31.0
+			ERR_m_g[l] = tbdata_FDS.field('g_mag_e')[j]
 			R_eff[l] = tbdata_FDS.field('reff')[j]
 			ERR_R_eff[l] = tbdata_FDS.field('reff_e')[j]
 			axis_ratio[l] = tbdata_FDS.field('arat')[j]
@@ -90,14 +95,15 @@ ListDict = []
 for l in range(size):
 	draft = name_list[l]
 	dict={'FCC':draft[0], 'FDS':draft[1], '_DWARF':"{0:0=3d}".format(int(draft[2])), 'RA(deg)':RA[l], 'DEC(deg)':DEC[l],
-			 'M_r(mag)':"%.2f" % M_r[l], 'ERR_m_r(mag)':"%.2f" % ERR_m_r[l] ,
+			 'M_r(mag)':"%.2f" % M_r[l], 'ERR_m_r(mag)':"%.2f" % ERR_m_r[l], 'M_g(mag)':"%.2f" % M_g[l], 'ERR_m_g(mag)':"%.2f" % ERR_m_g[l] ,
 			'mu_r(mag/arcsec2)':"%.2f" % mu_r[l], 'ERR_mu_r(mag/arcsec2)':"%.2f" % ERR_mu_r[l] , 
 			'R_e(arcsec)':"%.2f" % R_eff[l], 'ERR_R_e(arcsec)':"%.2f" % ERR_R_eff[l] , 
 			'axis_ratio':"%.2f" % axis_ratio[l], 'Sersic_index':"%.2f" % sersic_index[l],'log10(M_*/M_sun)':"%.4f" % log_mass[l], 'ERR_log10(M_*)':"%.4f" % ERR_log_mass[l],
 			'u':"%.4f" % u[l], 'g':"%.4f" % g[l], 'r':"%.4f" % r[l], 'i':"%.4f" % i[l]}
 	ListDict.append(dict.copy())
 
-with open('../2_pipeline/0_Galaxies_Table/Galaxies_Table.csv', 'w') as output_file:
+#with open('../2_pipeline/0_Galaxies_Table/Galaxies_Table.csv', 'w') as output_file:
+with open('../2_pipeline/0_Galaxies_Table/tmp/NotChosen_Galaxies_Table.csv', 'w') as output_file:
     dict_writer = csv.DictWriter(output_file, dict.keys())
     dict_writer.writeheader()
     dict_writer.writerows(ListDict)
